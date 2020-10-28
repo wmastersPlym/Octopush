80385d555ee496964975f67661b1d7ac06033aea
wmastersPlym <43572947+wmastersPlym@users.noreply.github.com>
2020-10-28 22:26:20 +0000

    Added baisc underwater physics for puck
    Added followMouse script so user can use the stick to interact with the puck to test physics

---
+++ OctoPush - The Game/Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    private Vector3 mousePos;
+    public float moveSpeed = 3.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            //transform.position = Vector2.Lerp(transform.position, mousePos, moveSpeed);
+            GetComponent<Rigidbody2D>().MovePosition(mousePos);
+        }
+        if (Input.GetMouseButton(1))
+        {
+            gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+        } else
+        {
+            gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+        }
+        if(Input.GetKey(KeyCode.Space))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 1f));
+        }
+
+    }
+}
