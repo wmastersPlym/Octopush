d827a07154947480ec687d8241ba4379ad765216
wmastersPlym <43572947+wmastersPlym@users.noreply.github.com>
2021-03-21 19:37:21 +0000

    Added player art
    Added first iteration of the players character art

--- OctoPush - The Game/Assets/Scripts/PlayerController.cs
+++ OctoPush - The Game/Assets/Scripts/PlayerController.cs
@@ -39,18 +39,29 @@ public class PlayerController : MonoBehaviour
         {
             currentSpeed = swimSpeed;
         }
-        
+
+        float xMove = 0.0f;
+        float yMove = 0.0f;
+
         if(Input.GetAxisRaw("Horizontal") > 0.1f || Input.GetAxisRaw("Horizontal") < -0.1f)
         {
             //rb.velocity = new Vector2(currentSpeed* Input.GetAxis("Horizontal"), rb.velocity.y);
-            transform.position += new Vector3(Input.GetAxis("Horizontal") * currentSpeed * Time.deltaTime, 0.0f, 0.0f);
+            //transform.position += new Vector3(Input.GetAxis("Horizontal") * currentSpeed * Time.deltaTime, 0.0f, 0.0f);
+            xMove = Input.GetAxis("Horizontal") * currentSpeed * Time.deltaTime;
         }
 
         if (Input.GetAxisRaw("Vertical") > 0.1f || Input.GetAxisRaw("Vertical") < -0.1f)
         {
             //rb.velocity = new Vector2(rb.velocity.x, Input.GetAxis("Vertical") * currentSpeed);
-            transform.position += new Vector3(0.0f, Input.GetAxis("Vertical") * currentSpeed * Time.deltaTime, 0.0f);
+            //transform.position += new Vector3(0.0f, Input.GetAxis("Vertical") * currentSpeed * Time.deltaTime, 0.0f);
+            yMove = Input.GetAxis("Vertical") * currentSpeed * Time.deltaTime;
         }
+
+        if(xMove != 0 || yMove != 0)
+        {
+            rb.MovePosition(new Vector2(transform.position.x + xMove, transform.position.y + yMove));
+        }
+
 
 
     }
